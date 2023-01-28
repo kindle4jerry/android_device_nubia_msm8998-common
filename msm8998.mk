ace2d307e9093517494e2a7b77abbc090c553c2f
kindle4jerry <kindle4jerry@gmail.com>
2023-01-28 10:22:31 +0800

    PE-ify-2

--- msm8998.mk
+++ msm8998.mk
@@ -16,8 +16,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
